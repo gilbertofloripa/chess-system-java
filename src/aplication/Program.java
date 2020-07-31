26e3dfd47e8c0024b78138a9fbc67b7b0b38bff4
gilbertpfloripa <gilberto_floripa@hotmail.com>
2020-07-31 17:38:14 -0500

    Projeto criado

---
+++ src/aplication/Program.java
@@ -0,0 +1,15 @@
+package aplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(LOcale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+
+}
